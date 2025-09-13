0b3b19251dfff4d62c5402f660328a53ec61155d
Anna-Julia Seppänen <anna-julia.seppanen@hotmail.com>
2025-09-13 13:44:08 +0200

    Reviews,Gropus,Profile. CreateAccount popup

--- web/src/screens/App.jsx
+++ web/src/screens/App.jsx
@@ -2,6 +2,9 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from '../components/navBar'
 import Movies from "./Movies"
+import Groups from './Groups'
+import Reviews from './Reviews'
+import Profile from './Profile'
 import Home from './Home';
 import SearchBar from '../components/SearchBar';
 
@@ -17,6 +20,9 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/movies" element={<Movies />}></Route>
+            <Route path="/groups" element={<Groups />} />
+            <Route path='/reviews' element={<Reviews />} />
+            <Route path='/profile' element={<Profile />} />
         </Routes>
       </div>
     </>
