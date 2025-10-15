04772a6fd02b96f6479253b30c2b0ca1f7dbbed8
j3shi <jebuyt@gmail.com>
2025-10-15 20:11:28 +0300

    index.js and db.js fixes for render deployment

--- web/server/index.js
+++ web/server/index.js
@@ -18,6 +18,11 @@ app.use(cors())                 //sallitaan CORS
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 
+// Root route for Render
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
 //Reitit
 app.use('/api/users', userRouter)
 app.use('/api/reviews', reviewRouter)
